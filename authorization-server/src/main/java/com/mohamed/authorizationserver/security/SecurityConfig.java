e462ca33e875cfe83f86bd688594520e68b366c1
Mohamed Tarek <mohamedtarek3.2001@gmail.com>
2024-10-02 18:30:44 +0300

    add AuthorizationServerSettings

--- authorization-server/src/main/java/com/mohamed/authorizationserver/security/SecurityConfig.java
+++ authorization-server/src/main/java/com/mohamed/authorizationserver/security/SecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ConfigurationSettingNames;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -95,5 +97,11 @@ public class SecurityConfig {
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .build();
         return new InMemoryRegisteredClientRepository(client);
+    }
+
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
     }
 }
